7b4b3fcc6139ebbb1318b9a54ea9d475f0322eb1
Juhee Hong <juheehong311@gmail.com>
2022-06-09 13:37:50 +0000

    finish about/photography/portfolio sections

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,19 +16,30 @@
  * Adds a random quote to the page.
  */
 function addRandomQuote() {
-  const quotes =
-      ['A rock pile ceases to be a rock pile the moment a single man contemplates it, bearing within him the image of a cathedral.', 
-      'Happiness can be found, even in the darkest of times, if one only remembers to turn on the light.', 
-      'All we have to decide is what to do with the time that is given us.', 
-      'Go where you must go, and hope!',
-      "I must endure the presence of a few caterpillars if I wish to become acquainted with the butterflies.",
-      "What makes the desert beautiful is that somewhere it hides a well.",
-      ];
+    const quotes =
+        ['A rock pile ceases to be a rock pile the moment a single man contemplates it, bearing within him the image of a cathedral.', 
+        'Happiness can be found, even in the darkest of times, if one only remembers to turn on the light.', 
+        'All we have to decide is what to do with the time that is given us.', 
+        'Go where you must go, and hope!',
+        "I must endure the presence of a few caterpillars if I wish to become acquainted with the butterflies.",
+        "What makes the desert beautiful is that somewhere it hides a well.",
+        "You never walk alone.",
+        ];
+  
+    // Pick a random greeting.
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  
+    // Add it to the page.
+    const quoteContainer = document.getElementById('quote-container');
+    quoteContainer.innerText = quote;
+  }
 
-  // Pick a random greeting.
-  const quote = quotes[Math.floor(Math.random() * quotes.length)];
-
-  // Add it to the page.
-  const quoteContainer = document.getElementById('quote-container');
-  quoteContainer.innerText = quote;
-}
+  
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+}, 5000);
